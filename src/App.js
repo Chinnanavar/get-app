5c8df9ea2c838e66ba62ec96e371a0be8c8cb275
@Chinnannavar <yashchinnannavar18@gmail.com.>
2025-01-24 12:16:42 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import Header from "./Components/Header/Header";
-import About from "./Pages/About";
-import Conatct from "./Pages/Conatct";
-import Service1 from "./Pages/Service1";
-import Footer from "./Components/Footer/Footer";
+import App3 from "./App3";
+// import Home from "./Pages/Home";
+// import Header from "./Components/Header/Header";
+// import About from "./Pages/About";
+// import Conatct from "./Pages/Conatct";
+// import Service1 from "./Pages/Service1";
+// import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/service" element={<Service1 />} />
-        <Route path="/contact" element={<Conatct />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Header />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/service" element={<Service1 />} />
+    //     <Route path="/contact" element={<Conatct />} />
+    //   </Routes>
+    //   <Footer />
+    // </BrowserRouter>
+    <App3 />
   );
 };
 
